619cd570c1452fc509acd878406e1987c38dc9d4
wsstssw <o287200812@sohu.com>
2016-11-20 13:48:27 +0800

    项目建立

---
+++ ZZCamera/ZZCamera/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZZCamera
+//
+//  Created by 赵瑞玮 on 16/11/20.
+//  Copyright © 2016年 zhaoruiwei. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
